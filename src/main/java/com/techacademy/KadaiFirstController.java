dc740bc834032d348852aa7920345fe1cd2f17e0
YUKA <YUKA@localhost>
2023-04-21 22:27:58 +0900

    コメントの追加

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KadaiFirstController {
 
+    // 指定日の曜日を算定する
     @GetMapping("/dayofweek/{date}")
     public String dispDayOfWeek(@PathVariable String date) {
 
@@ -26,24 +27,33 @@ public class KadaiFirstController {
         return week.toString();
     }
 
+    // 四則演算を行なう
+
+    // 足し算
     @GetMapping("/plus/{val1}/{val2}")
     public int calcPlus(@PathVariable int val1, @PathVariable int val2) {
         int res = 0;
         res = val1 + val2;
         return res;
     }
+
+    // 引き算
     @GetMapping("/minus/{val1}/{val2}")
     public int calcMinus(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
         res = val1 - val2;
         return res;
     }
+
+    // 掛け算
     @GetMapping("/times/{val1}/{val2}")
     public int calcTimes(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
         res = val1 * val2;
         return res;
     }
+
+    // 割り算
     @GetMapping("/divide/{val1}/{val2}")
     public int calcDivide(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
